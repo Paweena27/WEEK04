4d60d4c36bb9a8f7989de4a2616655d6ffdfb60f
Paweena27 <59030185@kmitl.ac.th>
2017-09-07 15:05:29 +0700

    W04P08

--- lab4/lab4/Program.cs
+++ lab4/lab4/Program.cs
@@ -7,7 +7,12 @@ namespace lab4
     {
         static void Main(string[] args)
         {
-            
+            const double i = 123.456789d;
+            Console.WriteLine("{0:F1}", i);
+            Console.WriteLine("{0:F2}", i);
+            Console.WriteLine("{0:F3}", i);
+            Console.WriteLine("{0:F4}", i);
+            Console.WriteLine("{0:F5}", i);
         }
     }
 }
