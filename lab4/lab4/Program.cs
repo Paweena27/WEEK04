89dcaf3ca67a43760cf9b6b6eb23842bb26e1bba
Paweena27 <59030185@kmitl.ac.th>
2017-09-07 14:27:26 +0700

    W04P06

--- lab4/lab4/Program.cs
+++ lab4/lab4/Program.cs
@@ -7,7 +7,15 @@ namespace lab4
     {
         static void Main(string[] args)
         {
-            
+            int n = 123456789;
+            Console.WriteLine("{0, 0:E}", n);
+            Console.WriteLine("{0, 0:F}", n);
+            Console.WriteLine("{0, 0:G}", n);
+            Console.WriteLine("{0, 0:N}", n);
+            Console.WriteLine("{0, 0:P}", n);
+            Console.WriteLine("{0, 0:X}", n);
+            Console.ReadKey();
+
         }
     }
 }
