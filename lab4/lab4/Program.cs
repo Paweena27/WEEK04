e6edc64495f30a673e815b6eb9f59653efa5a44c
Paweena27 <59030185@kmitl.ac.th>
2017-09-07 15:15:32 +0700

    W04P11

--- lab4/lab4/Program.cs
+++ lab4/lab4/Program.cs
@@ -7,7 +7,11 @@ namespace lab4
     {
         static void Main(string[] args)
         {
-            
+            string str;
+            Console.Write("Please enter value 1 : ");
+            int val1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(val1);
+
         }
     }
 }
